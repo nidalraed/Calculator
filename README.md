6aef371c4af32ef6910dfb581341d1aa53610706
nidalraed4 <raednidal556@gmail.com>
2024-03-26 19:57:37 +0300

    Removed Pervious commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Calculator By Nidal Raed
+
+- # live Demo : [https://nidalraed.github.io/Calculator/]
---
+++ calculator.js
@@ -0,0 +1,36 @@
+// // calculator program
+
+// const display=document.getElementById("display");
+
+// function appendToDisplay(input){
+//     display.value+=input;
+
+// }
+// function clearDisplay(){
+//     display.value="";
+
+// }
+// function calculate(){
+//     try{
+//         display.value=eval(display.value);
+//     }
+//     catch(error){
+//         display.value="Error";
+//     }
+    
+// }
+
+const display=document.getElementById('display');
+function appendToDisplay(input){
+    display.value+=input;
+}
+function calculate(){
+    try {
+        display.value=eval(display.value);
+    } catch (error) {
+        display.value="Error";
+    }
+}
+function clearDisplay(){
+    display.value="";
+}
